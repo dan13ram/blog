71baceb0235c0e18de5bfc8b6024631452d2b41f
Dhanwanthari Ramakrishnan <dan13ram@gmail.com>
2020-05-17 19:59:40 +0530

    setup flamelink with gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,83 +1,105 @@
+require("dotenv").config()
+const parseNewLines = function (key) {
+    return typeof key === "string" ? key.replace(/\\n/g, "\n") : key
+}
+const privateKey = parseNewLines(process.env.FIREBASE_PRIVATE_KEY)
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+    siteMetadata: {
+        title: `Gatsby Starter Blog`,
+        author: {
+            name: `Kyle Mathews`,
+            summary: `who lives and works in San Francisco building useful things.`
+        },
+        description: `A starter blog demonstrating what Gatsby can do.`,
+        siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+        social: {
+            twitter: `kylemathews`
+        }
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
-    social: {
-      twitter: `kylemathews`,
-    },
-  },
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              maxWidth: 590,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
+                path: `${__dirname}/content/blog`,
+                name: `blog`
+            }
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
-    },
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+                path: `${__dirname}/content/assets`,
+                name: `assets`
+            }
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 590
+                        }
+                    },
+                    {
+                        resolve: `gatsby-remark-responsive-iframe`,
+                        options: {
+                            wrapperStyle: `margin-bottom: 1.0725rem`
+                        }
+                    },
+                    `gatsby-remark-prismjs`,
+                    `gatsby-remark-copy-linked-files`,
+                    `gatsby-remark-smartypants`
+                ]
+            }
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                //trackingId: `ADD YOUR TRACKING ID HERE`,
+            }
+        },
+        `gatsby-plugin-feed`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Gatsby Starter Blog`,
+                short_name: `GatsbyJS`,
+                start_url: `/`,
+                background_color: `#ffffff`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `content/assets/gatsby-icon.png`
+            }
+        },
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-plugin-typography`,
+            options: {
+                pathToConfigModule: `src/utils/typography`
+            }
+        },
+        {
+            resolve: "gatsby-source-flamelink",
+            options: {
+                firebaseConfig: {
+                    privateKey,
+                    databaseURL: process.env.FIREBASE_DATABASE_URL, 
+                    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+                    projectId: process.env.FIREBASE_PROJECT_ID,
+                    clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+                },
+                environment: "production",
+                content: true,
+                populate: true,
+                navigation: true,
+                globals: true
+            }
+        }
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ]
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,89 +8,104 @@ import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 
 type Data = {
-  site: {
-    siteMetadata: {
-      title: string
+    site: {
+        siteMetadata: {
+            title: string
+        }
     }
-  }
-  allMarkdownRemark: {
-    edges: {
-      node: {
-        excerpt: string
-        frontmatter: {
-          title: string
-          date: string
-          description: string
-        }
-        fields: {
-          slug: string
-        }
-      }
-    }[]
-  }
+    allMarkdownRemark: {
+        edges: {
+            node: {
+                excerpt: string
+                frontmatter: {
+                    title: string
+                    date: string
+                    description: string
+                }
+                fields: {
+                    slug: string
+                }
+            }
+        }[]
+    }
 }
 
 const BlogIndex = ({ data, location }: PageProps<Data>) => {
-  const siteTitle = data.site.siteMetadata.title
-  const posts = data.allMarkdownRemark.edges
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMarkdownRemark.edges
 
-  return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
-      <Bio />
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
-        return (
-          <article key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </section>
-          </article>
-        )
-      })}
-    </Layout>
-  )
+    return (
+        <Layout location={location} title={siteTitle}>
+            <SEO title="All posts" />
+            <h1>{data.allFlamelinkPostContent.edges[0].node.title}</h1>
+            <h2>{data.allFlamelinkPostContent.edges[0].node.content}</h2>
+            <Bio />
+            {posts.map(({ node }) => {
+                const title = node.frontmatter.title || node.fields.slug
+                return (
+                    <article key={node.fields.slug}>
+                        <header>
+                            <h3
+                                style={{
+                                    marginBottom: rhythm(1 / 4)
+                                }}
+                            >
+                                <Link
+                                    style={{ boxShadow: `none` }}
+                                    to={node.fields.slug}
+                                >
+                                    {title}
+                                </Link>
+                            </h3>
+                            <small>{node.frontmatter.date}</small>
+                        </header>
+                        <section>
+                            <p
+                                dangerouslySetInnerHTML={{
+                                    __html:
+                                        node.frontmatter.description ||
+                                        node.excerpt
+                                }}
+                            />
+                        </section>
+                    </article>
+                )
+            })}
+        </Layout>
+    )
 }
 
 export default BlogIndex
 
 export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
+    query {
+        site {
+            siteMetadata {
+                title
+            }
         }
-      }
+        allFlamelinkPostContent {
+            edges {
+                node {
+                    title
+                    content
+                }
+            }
+        }
+        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+            edges {
+                node {
+                    excerpt
+                    fields {
+                        slug
+                    }
+                    frontmatter {
+                        date(formatString: "MMMM DD, YYYY")
+                        title
+                        description
+                    }
+                }
+            }
+        }
     }
-  }
 `
